f8044ebb035519c2ee9c0ec7379a564e62bdad19
Ramesh Chellaton <chellaton@yahoo.com>
2019-09-09 13:00:34 -0400

    check in - temp

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-print("hello")
+makeCacheMatrix <- function(aMatrix = matrix()) {
+  invMatrix <- NULL
+  set <- function(y) {
+    aMatrix <<- y
+    invMatrix <<- NULL
+  }
+  get <- function() aMatrix 
+  getInverse <- function() invMatrix
+  setInverse <- function() {
+    invMatrix <<- solve(m <<- aMatrix)
+  }
+
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
